0de03ecaa06178d68dd4b0ad5875ef9df0910a8a
thembones79 <michal.szumnarski@gmail.com>
2019-03-26 22:35:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+/*
+ * Primary file for the api
+ *
+ *
+ *
+ *
+ */
+
+// Dependiences
+
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+
+// The server should respond to all request wuth a string
+const server = http.createServer(function(req, res) {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get the HTTP Method
+  const method = req.method.toLocaleLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder("utf-8");
+  const buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function(){
+      buffer += decoder.end();
+  });
+
+  // Send the response
+  res.end("Hello!\n");
+
+  // Log request path
+  console.log("Request received with these headers: ", headers);
+});
+
+// Start the server, and have it listen on port 3000
+
+server.listen(3000, function() {
+  console.log("The server is listening on port 3000 now");
+});
